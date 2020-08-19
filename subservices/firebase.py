6ec336c3aa343f70b4d50d0510ba527064891124
Fyodor Sizov <f.sizov@yandex.ru>
2020-08-19 13:56:12 +0300

    google_auth route

--- subservices/firebase.py
+++ subservices/firebase.py
@@ -9,3 +9,8 @@ firebase = Blueprint("firebase", __name__, template_folder="templates")
 @firebase.route("/facebook_auth")
 def facebook_auth():
     return render_template("firebase_auth.html")
+
+
+@firebase.route("/google_auth")
+def facebook_auth():
+    return render_template("google_auth.html")
