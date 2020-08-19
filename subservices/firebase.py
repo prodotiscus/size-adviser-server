46009aad93e19da550ce4f6512897cc2a0935b7c
Fyodor Sizov <f.sizov@yandex.ru>
2020-08-19 14:30:06 +0300

    set/get session data

--- subservices/firebase.py
+++ subservices/firebase.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 
+from flask import abort
 from flask import Blueprint
+from flask import jsonify
+from flask import request
 from flask import render_template
+
+import sqlite3
 
 firebase = Blueprint("firebase", __name__, template_folder="templates")
 
@@ -14,3 +19,49 @@ def facebook_auth():
 @firebase.route("/google_auth")
 def google_auth():
     return render_template("google_auth.html")
+
+
+@firebase.route("/set_session_data")
+def set_session_data():
+    session_id = request.args.get("session_id")
+    user_email = request.args.get("user_email")
+    user_name = request.args.get("user_name")
+    if not session_id or not user_email or not user_name:
+        return abort(400)
+    v = [session_id, user_email, user_name]
+    db = sqlite3.connect("databases/personal.sqlite3")
+    c = db.cursor()
+    c.execute("INSERT INTO firebase_sessions VALUES (?, ?, ?)", v)
+    db.commit()
+    db.close()
+
+
+@firebase.route("/get_session_data")
+def get_session_data():
+    session_id = request.args.get("session_id")
+    preserve_row = request.args.get("preserve", False)
+    if not session_id:
+        return abort(400)
+
+    db = sqlite3.connect("databases/personal.sqlite3")
+    c = db.cursor()
+    d = c.execute("SELECT user_email, user_name FROM firebase_sessions WHERE session_id='%s'" % session_id).fetchone()
+    try:
+        d = d[0]
+    except IndexError:
+        return abort(400)
+    if d is None:
+        return abort(400)
+
+    if not preserve_row:
+        c.execute("DELETE FROM firebase_sessions WHERE session_id='%s'" % session_id)
+    db.commit()
+    db.close()
+
+    return jsonify({
+        "status": "success",
+        "data": {
+            "email": d[0],
+            "name": d[1]
+        }
+    })
