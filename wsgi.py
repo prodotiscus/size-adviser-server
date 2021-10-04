00f9f21eaf22af93bc1b20001aba9e97afd12c3e
Fyodor Sizov <f.sizov@yandex.ru>
2021-10-04 06:03:18 +0300

    logger

--- wsgi.py
+++ wsgi.py
@@ -3,8 +3,10 @@
 from index import app
 import logging
 
+gunicorn_error_logger = logging.getLogger('gunicorn.error')
+app.logger.handlers.extend(gunicorn_error_logger.handlers)
+app.logger.setLevel(logging.DEBUG)
+app.logger.debug('this will show in the log')
+
 if __name__ == "__main__":
-    handler = logging.FileHandler('/var/log/size-adviser.error.log')
-    handler.setLevel(logging.ERROR)
-    app.logger.addHandler(handler)
-        app.run()
+    app.run()
