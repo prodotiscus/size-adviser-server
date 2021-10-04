848324cfdbf1978269d2ceb2d339bc42c988009f
Fyodor Sizov <f.sizov@yandex.ru>
2021-10-04 05:55:29 +0300

    enable logging

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 from index import app
+import logging
 
 if __name__ == "__main__":
+    handler = logging.FileHandler('/var/log/size-adviser.error.log')
+    handler.setLevel(logging.ERROR)
+    app.logger.addHandler(handler)
         app.run()
